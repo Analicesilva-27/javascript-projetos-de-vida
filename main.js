759dccece879fd4e518ffd642fc8f75459d43fbc
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2025-07-01 08:42:09 -0300

    dom

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelector(".botao");
+
+for(let i =0;i<botoes.lenght; i++){
+    botoes(I).ONCLICK=function(){
+        botoes[j].classlist.remove('ativo')
+    }
+botoes[i].classlist.add()
+}
