8facc0429fedc86a9239d5eae5e36357adffac36
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2025-07-29 07:58:48 -0300

    retorno

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i = 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
@@ -14,6 +14,21 @@ for(let i = 0; i< botoes.length; i++){
 
 const contadores= document.querySelectorAll(".contador");
 const tempoObjetivo1= new date ("2025-10-05T00:00:00");
-let tempoAtual = new date ();
 
 contadores [0].textcontent = tempoObjetivo1-tempoAtual;
+
+function calculatempo(tempoObjetivo){
+    let tempoAtual = new date ();
+    let tempo final = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas  = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+  
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos"
+
+}
